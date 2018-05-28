fce18882f1ee7e375bc98c57c26bca99145810a7
Mikhail <madion86@yandex.ru>
2018-05-28 21:16:59 +0300

    3rd task first part

--- task3.js
+++ task3.js
@@ -10,3 +10,18 @@
 > node task.rb 3600 3599
 > 1 час 59 минут 59 секунд
 */
+
+var t1 = parseFloat(process.argv[2]);
+var t2 = parseFloat(process.argv[3]);
+
+if (t1>=1 && t1<=100000 && t2>=1 && t2<=100000) {
+    var t = t1+t2;
+    var hours = Math.trunc(t/3600);
+    var minutes = Math.trunc((t - hours*3600)/60);
+    var seconds = t%60;
+}
+
+console.log(hours,minutes,seconds);
+
+// Привести к человеческому виду. Если разряд пропущен, то не выводить его вообще!
+
