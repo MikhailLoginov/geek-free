e2e47ee5657c4694cdd32a1c59a7a06609c00ad4
Shadow <madion86@yandex.ru>
2018-06-02 00:02:43 +0300

    task1 fixed

--- task1.js
+++ task1.js
@@ -15,17 +15,28 @@ var i = parseFloat(process.argv[2]),
     normal = 0,
     result = "";
 
-if (i>=0&&i<=10000 && a>=1&&a<=100&& b>=1&&b<=100) {
-  for (let j=0; j<String(i).length; j++) {
-    normal += +String(i)[j]*Math.pow(a, String(i).length-1-j);
-  }
+// if (i>=0&&i<=10000 && a>=1&&a<=100&& b>=1&&b<=100) {
+for (let j=0; j<String(i).length; j++) {
+  normal += +String(i)[j]*Math.pow(a, String(i).length-1-j);
+}
+console.log(normal);
   
+while (normal>=b) {
+  result = String(normal % b) + result;
+  normal = Math.trunc(normal/b);
+  console.log(normal);
+}
+result = normal + result;
+
+
+  /*
   while (normal/b>=1) {
     result += String(Math.trunc(normal/b));
     normal -= Math.trunc(normal/b)*b;
   }
   result += String(normal);
-  process.stdout.write(result);
-}
+  */
+process.stdout.write(result);
+
 
 
