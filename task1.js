d7483b8cbf45d3eb93112f61ad14dfbc2ca0b6d4
Shadow <madion86@yandex.ru>
2018-06-03 20:43:18 +0300

    task1 lower case

--- task1.js
+++ task1.js
@@ -14,14 +14,14 @@ var i = process.argv[2],
     b = parseFloat(process.argv[4]),
     normal = 0,
     result = "",
-    numberTranslation = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', "G", 
-    'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    numberTranslation = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', "g", 
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // if (i>=0&&i<=10000 && a>=1&&a<=100&& b>=1&&b<=100) {
 for (let j=0; j<i.length; j++) {
   let number;
-  for (number=0; number<numberTranslation.length & i[j] != numberTranslation[number]; number++) {};
-  normal += +number*Math.pow(a, String(i).length-1-j);
+  for (number=0; number<numberTranslation.length & i[j].toLowerCase() != numberTranslation[number]; number++) {};
+  normal += number*Math.pow(a, i.length-1-j);
 }
   
 while (normal>=b) {
